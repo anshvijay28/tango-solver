ef0ccf123a8b4601ac40e676db9ef68110528df8
Ansh Vijay <anshvijay28@gmail.com>
2025-01-02 13:17:21 -0800

    got tailwind to work and resolved web vitals issue

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className=""
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
     </div>
   );
 }
