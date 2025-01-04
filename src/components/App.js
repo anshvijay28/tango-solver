5d34c538b59dc9d9398df5bce31e961b28a9f3cb
Ansh Vijay <anshvijay28@gmail.com>
2025-01-04 01:04:22 -0800

    border clicks will now not change the value of a cell

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import '../styles/App.css';
 import Cell from '../components/Cell.js';
 
-
 function App() {
 
   return (
@@ -10,6 +9,7 @@ function App() {
         {Array.from({ length: 36 }).map((_, index) => (
           <Cell key={index} bgColor={"#F9F9F9"} />
         ))}
+        {/* <Cell bgColor={"#F9F9F9"}/> */}
       </div>
     </div>
   );
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const CELL_SIZE = 55;
+export const LOGO_SIZE = 24;
+export const SYMBOL_SPACE = 2;
+export const SYMBOL_OFFSET = 20;
