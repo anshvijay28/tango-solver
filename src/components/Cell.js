a5f20faddf13d17dbfdf15e34e30d009c5a3a7f2
Ansh Vijay <anshvijay28@gmail.com>
2025-01-09 14:32:03 -0500

    symbol UI and clickablilty finalized. board is now fully customizable

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,6 @@ import "../styles/Cell.css";
 import moon from "../images/moon.png";
 import {
 	CELL_SIZE,
-	LOGO_SIZE,
 	SYMBOL_SPACE,
 	SYMBOL_OFFSET,
 } from "../constants";
@@ -55,68 +54,87 @@ function Cell({ bgColor, row, col, signs, setSigns }) {
 	const handleClick = (e) => {
 		const rect = e.target.getBoundingClientRect();
 
-		if (rect.width === LOGO_SIZE && rect.height === LOGO_SIZE) {
-			setClicks((clicks + 1) % 3);
+		const x = e.clientX - rect.left;
+		const y = e.clientY - rect.top;
+
+		const north =
+			x >= SYMBOL_OFFSET && x <= CELL_SIZE - SYMBOL_OFFSET && y <= SYMBOL_SPACE;
+		const south =
+			x >= SYMBOL_OFFSET &&
+			x <= CELL_SIZE - SYMBOL_OFFSET &&
+			y >= CELL_SIZE - SYMBOL_SPACE;
+		const west =
+			x <= SYMBOL_SPACE && y >= SYMBOL_OFFSET && y <= CELL_SIZE - SYMBOL_OFFSET;
+		const east =
+			x >= CELL_SIZE - SYMBOL_SPACE &&
+			y >= SYMBOL_OFFSET &&
+			y <= CELL_SIZE - SYMBOL_OFFSET;
+
+		const directions = { north, south, west, east };
+		const dir = Object.keys(directions).find((key) => directions[key]);
+
+		// some bounds checking for edge clicks
+		if (north || south || west || east) {
+			// bounds check
+			if (!boundsCheck(row, col, dir, signs)) return;
+
+			const newSigns = signs.map((row) => [...row]);
+
+			// updating current square
+			const side = dirToSide[dir];
+			changeSigns(row, col, side, newSigns);
+
+			// updating neighbor square
+			const neiSide = dirToNeiSide[dir];
+			const [dr, dc] = dirToNeiInc[dir];
+			changeSigns(row + dr, col + dc, neiSide, newSigns);
+
+			setSigns(newSigns);
 		} else {
-			const x = e.clientX - rect.left;
-			const y = e.clientY - rect.top;
-
-			const north =
-				x >= SYMBOL_OFFSET &&
-				x <= CELL_SIZE - SYMBOL_OFFSET &&
-				y <= SYMBOL_SPACE;
-			const south =
-				x >= SYMBOL_OFFSET &&
-				x <= CELL_SIZE - SYMBOL_OFFSET &&
-				y >= CELL_SIZE - SYMBOL_SPACE;
-			const west =
-				x <= SYMBOL_SPACE &&
-				y >= SYMBOL_OFFSET &&
-				y <= CELL_SIZE - SYMBOL_OFFSET;
-			const east =
-				x >= CELL_SIZE - SYMBOL_SPACE &&
-				y >= SYMBOL_OFFSET &&
-				y <= CELL_SIZE - SYMBOL_OFFSET;
-
-			const directions = { north, south, west, east };
-			const dir = Object.keys(directions).find((key) => directions[key]);
-
-			// some bounds checking for edge clicks
-			if (north || south || west || east) {
-				// bounds check
-				if (!boundsCheck(row, col, dir, signs)) return;
-
-				const newSigns = signs.map((row) => [...row]);
-
-				// updating current square
-				const side = dirToSide[dir];
-				changeSigns(row, col, side, newSigns);
-
-				// updating neighbor square
-				const neiSide = dirToNeiSide[dir];
-				const [dr, dc] = dirToNeiInc[dir];
-				changeSigns(row + dr, col + dc, neiSide, newSigns);
-
-				setSigns(newSigns);
-			} else {
-				setClicks((clicks + 1) % 3);
-			}
+			setClicks((clicks + 1) % 3);
 		}
 	};
 
 	return (
 		<button
 			onClick={handleClick}
-			className="bg-gray-500 border flex items-center justify-center"
+			className="bg-gray-500 border flex items-center justify-center relative"
 			style={{ backgroundColor: bgColor }}
 		>
-			{clicks === 0 && <div className="w-6 h-6 rounded-full"></div>}
+			{clicks === 0 && (
+				<div
+					style={{ pointerEvents: "none" }}
+					className="w-6 h-6 rounded-full"
+				></div>
+			)}
 			{clicks === 1 && (
-				<div className="w-6 h-6 rounded-full bg-yellow-400 border-2 border-yellow-500"></div>
+				<div
+					style={{ pointerEvents: "none" }}
+					className="w-6 h-6 rounded-full bg-yellow-400 border-2 border-yellow-500"
+				></div>
 			)}
 			{clicks === 2 && (
-				<div className="w-6 h-6">
+				<div style={{ pointerEvents: "none" }} className="w-6 h-6">
 					<img src={moon} alt="moon.png" />
+				</div>
+			)}
+
+			{/* top = */}
+			{row > 0 && signs[row][col].top > 0 && (
+				<div
+					style={{ pointerEvents: "none" }}
+					className="absolute -top-2 left-1/2 -translate-x-1/2 z-20 text-xs font-bold bg-white rounded-full px-1 leading-[0.75rem]"
+				>
+					{signs[row][col].top === 1 ? `=` : `×`}
+				</div>
+			)}
+			{/* right */}
+			{col < signs[0].length - 1 && signs[row][col].right > 0 && (
+				<div
+					style={{ pointerEvents: "none" }}
+					className="absolute -right-2 z-20 text-xs font-bold bg-white rounded-full px-1"
+				>
+					{signs[row][col].right === 1 ? `=` : `×`}
 				</div>
 			)}
 		</button>
